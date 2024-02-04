ccf5ddf2ee4aa4d9eaf98423982a423ab35be294
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2024-02-04 12:15:45 -0300

    Create exercicio1.py

---
+++ python_iniciante/exercicio1.py
@@ -0,0 +1,7 @@
+num_int = input("Digite um número *inteiro* e vou lhe disse se ele é impar ou par: ")
+
+
+if int(num_int) % 2 == 0:
+    print("Este número é par")
+else:
+    print("Este número é impar")
