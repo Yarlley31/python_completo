4b7f49c0ca866e1e8353492ef677e59b079bc7ae
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2024-02-05 10:12:01 -0300

    dia 05/02

---
+++ python_iniciante/aula33.py
@@ -0,0 +1,9 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'Yarlley Fernandes'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# string[3] = 'ABC'
+# print(outra_variavel)
+print(string.zfill(20))
