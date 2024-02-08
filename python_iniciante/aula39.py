78fc903ac28e0e7ae572936b321dc5164324ab1b
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2024-02-08 10:32:13 -0300

    Create aula39.py

---
+++ python_iniciante/aula39.py
@@ -0,0 +1,15 @@
+"""
+Itegrando strings com while
+"""
+#       012345678910
+nome = 'Luiz Otávio' # Interáveis
+novo_nome = ' '
+#     1110987654321
+# tamanho_nome = len(nome)
+# print(nome)
+# print(tamanho_nome)
+
+while nome <= nome[10]:
+    nome[0:+1]
+    print(nome)
+    novo_nome += '*L*'
