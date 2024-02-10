cc699bbf9e21cc6d65e21c9ea3f3a550121f30f3
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2024-02-10 17:22:02 -0300

    Update aula39.py

--- python_iniciante/aula39.py
+++ python_iniciante/aula39.py
@@ -2,14 +2,16 @@
 Itegrando strings com while
 """
 #       012345678910
-nome = 'Luiz Otávio' # Interáveis
-novo_nome = ' '
+nome = 'YARLLEY FERNANDES' # Interáveis
 #     1110987654321
-# tamanho_nome = len(nome)
-# print(nome)
-# print(tamanho_nome)
 
-while nome <= nome[10]:
-    nome[0:+1]
-    print(nome)
-    novo_nome += '*L*'
+indice = 0
+novo_nome = ''
+
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
